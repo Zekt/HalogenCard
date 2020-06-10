c2cfa166337386de2ff68f0da3d128dad61290e7
Viktor Lin <hi@viktorl.in>
2020-06-10 18:21:27 +0000

    simple transition

--- src/Main.purs
+++ src/Main.purs
@@ -2,85 +2,48 @@ module Main where
 
 import Prelude
 
-import Affjax as AX
-import Affjax.ResponseFormat as AXRF
-import Data.Either (hush)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import Effect.Aff.Class (class MonadAff)
+import Halogen.HTML.CSS as HC
+import CSS as CSS
 import Halogen as H
-import Halogen.Aff (awaitBody, runHalogenAff)
+import Halogen.Aff as HA
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Halogen.VDom.Driver (runUI)
-import Web.Event.Event (Event)
-import Web.Event.Event as Event
+
+data State = Number
 
 main :: Effect Unit
-main = runHalogenAff do
-  body <- awaitBody
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
   runUI component unit body
 
-type State =
-  { loading :: Boolean
-  , username :: String
-  , result :: Maybe String
-  }
+data Action = Increment | Decrement
 
-data Action
-  = SetUsername String
-  | MakeRequest Event
-
-component :: forall q i o m. MonadAff m => H.Component HH.HTML q i o m
 component =
   H.mkComponent
     { initialState
     , render
     , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
     }
+  where
+  initialState _ = 10.0
 
-initialState :: forall i. i -> State
-initialState _ = { loading: false, username: "", result: Nothing }
+  render state =
+    HH.div_
+      [ HH.button [ HE.onClick \_ -> Just Decrement ] [ HH.text "-" ]
+      , HH.button [ HE.onClick \_ -> Just Increment ] [ HH.text "+" ]
+      , HH.div [ HC.style $ do CSS.border CSS.solid (CSS.px 5.0) CSS.red
+                               CSS.height (CSS.px 5.0)
+                               CSS.width  (CSS.px $ 5.0 + state)
+                               CSS.marginLeft (CSS.em 2.0)
+                               CSS.rule (CSS.Property (CSS.Key $ CSS.Plain "transition") (CSS.Value $ CSS.Plain "width 1s"))
+               ]
+               [ HH.text $ "Example" ]
+      ]
 
-render :: forall m. State -> H.ComponentHTML Action () m
-render st =
-  HH.form
-    [ HE.onSubmit \ev -> Just (MakeRequest ev) ]
-    [ HH.h1_ [ HH.text "Look up GitHub user" ]
-    , HH.label_
-        [ HH.div_ [ HH.text "Enter username:" ]
-        , HH.input
-            [ HP.value st.username
-            , HE.onValueInput \str -> Just (SetUsername str)
-            ]
-        ]
-    , HH.button
-        [ HP.disabled st.loading
-        , HP.type_ HP.ButtonSubmit
-        ]
-        [ HH.text "Fetch info" ]
-    , HH.p_
-        [ HH.text (if st.loading then "Working..." else "") ]
-    , HH.div_
-        case st.result of
-          Nothing -> []
-          Just res ->
-            [ HH.h2_
-                [ HH.text "Response:" ]
-            , HH.pre_
-                [ HH.code_ [ HH.text res ] ]
-            ]
-    ]
-
-handleAction :: forall o m. MonadAff m => Action -> H.HalogenM State Action () o m Unit
-handleAction = case _ of
-  SetUsername username -> do
-    H.modify_ _ { username = username, result = Nothing }
-
-  MakeRequest event -> do
-    H.liftEffect $ Event.preventDefault event
-    username <- H.gets _.username
-    H.modify_ _ { loading = true }
-    response <- H.liftAff $ AX.get AXRF.string ("https://api.github.com/users/" <> username)
-    H.modify_ _ { loading = false, result = map _.body (hush response) }
+  handleAction = case _ of
+    Increment -> H.modify_ \s -> s + 10.0
+    Decrement -> H.modify_ \s -> s - 10.0
